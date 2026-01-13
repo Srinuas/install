6de1b430e55dde67fcef028b02b170bf0cae935d
Srinuas <srinivasvanapalli0499@gmail.com>
2026-01-14 00:04:09 +0530

    Created k8s_cluster_setup.sh file

---
+++ k8s_cluster_setup.sh
@@ -0,0 +1,5 @@
+# check the details before execute
+# 
+export KOPS_STATE_STORE=s3://srinu.as
+kops create cluster --name srinu.k8s.local --zones us-east-2a,us-east-2b,us-east-2c --master-size c7i-flex.large --master-volume-size 30 --node-count 2 --node-volume-size 20 --node-size t3.micro --image=ami-0f5fcdfbd140e4ab7
+kops update cluster --name srinu.k8s.local --yes --admin
