fadd8b237e8daad845747906e1af119b21d945f1
Srinuas <srinivasvanapalli0499@gmail.com>
2026-01-14 00:02:11 +0530

    Created kops.sh file

---
+++ kops.sh
@@ -0,0 +1,6 @@
+# check the version is same on website
+# https://kops.sigs.k8s.io/getting_started/install/
+
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
