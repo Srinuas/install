8ba193cf7aee8ad4cf28550fafc62c4616ad3f56
srinuas <srinu.as@og.com>
2026-02-05 08:37:38 +0000

    changed to c7i

--- k8s_cluster_setup.sh
+++ k8s_cluster_setup.sh
@@ -1,5 +1,5 @@
 export KOPS_STATE_STORE=s3://srinu.as
-kops create cluster --name srinu.k8s.local --zones us-east-2a,us-east-2b,us-east-2c --master-size m7i-flex.large --master-count 1 --master-volume-size 25 --node-size c7i-flex.large --node-count 2 --node-volume-size 20 --image ami-0f5fcdfbd140e4ab7
+kops create cluster --name srinu.k8s.local --zones us-east-2a,us-east-2b,us-east-2c --master-size c7i-flex.large --master-count 1 --master-volume-size 25 --node-size c7i-flex.large --node-count 2 --node-volume-size 20 --image ami-0f5fcdfbd140e4ab7
 kops update cluster --name srinu.k8s.local --yes --admin
 
 #export KOPS_STATE_STORE=s3://srinu.as
