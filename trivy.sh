7a91a0717219f5fc563a491926b07530c06256ab
Srinuas <srinivasvanapalli0499@gmail.com>
2026-03-07 14:48:12 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,1 +1,17 @@
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.69.1
+# curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.69.1
+
+# 1. Trivy repo configuration file ni create cheyyi
+cat << EOF | sudo tee /etc/yum.repos.d/trivy.repo
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+
+# 2. Yum cache ni update cheyyi
+sudo yum makecache
+
+# 3. Trivy ni install cheyyi
+sudo yum install trivy -y
