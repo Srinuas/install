90a7339695abb1f77d854b411d828cd4242298f2
srinuas <srinu.as@og.com>
2026-04-07 15:29:11 +0000

    are you ready now???

--- k8s_cluster_setup.sh
+++ k8s_cluster_setup.sh
@@ -1,4 +1,19 @@
+# install kops
+# check the version is same on website
+# https://kops.sigs.k8s.io/getting_started/install/
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
+# install kubectl
+# check the version is same on website
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 # eu-north-1
+
 export KOPS_STATE_STORE=s3://srinu.ns
 kops create cluster --name srinu.k8s.local --zones eu-north-1a,eu-north-1b,eu-north-1c --master-size c7i-flex.large --master-count 1 --master-volume-size 25 --node-size c7i-flex.large --node-count 2 --node-volume-size 20 --image ami-0974a2c5ddf10f442
 kops update cluster --name srinu.k8s.local --yes --admin
