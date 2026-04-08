62196072ee1ef287341ad2f2ebb8caa1c06aabb0
Srinuas <srinivasvanapalli0499@gmail.com>
2026-04-08 10:51:25 +0530

    Add eksctl installation and cluster setup script

---
+++ eksctl_install.sh
@@ -0,0 +1,3 @@
+eksctl create cluster --name=Srinu_EKS --region=eu-north-1 --zones=eu-north-1a,eu-north-1b --without-nodegroup
+eksctl create nodegroup --cluster=Srinu_EKS --region=eu-north-1 --name=Srinu_node --node-type=c7i-flex.large --nodes=2 --nodes-min=2 --nodes-max=4 --node-volume-size=20 --ssh-access --ssh-public-key=pem --managed --asg-access --external-dns-access --full-ecr-access --appmesh-access --alb-ingress-access
+aws eks update-kubeconfig --name Srinu_EKS --region eu-north-1
